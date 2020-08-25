733cad65aa706175967e5d12d08588736bd69781
isebarn <isebarn182@gmail.com>
2020-08-25 18:34:07 +0000

    Prototype scraper

---
+++ betting/spiders/ORM.py
@@ -0,0 +1,74 @@
+import os
+import json
+from datetime import datetime
+from sqlalchemy import ForeignKey, desc, create_engine, func, Column, BigInteger, Integer, Float, String, Boolean, DateTime, Text
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from datetime import datetime
+
+engine = create_engine(os.environ.get('BETTING_DATABASE'), echo=False)
+Base = declarative_base()
+
+
+class MarathonFootballTeam(Base):
+  __tablename__ = 'marathon_football_team'
+
+  Id = Column('id', Integer, primary_key=True)
+  Value = Column('value', String)
+
+  def __init__(self, value):
+    self.Value = value
+
+class MarathonFootballMatch(Base):
+  __tablename__ = 'marathon_football_match'
+
+  Id = Column('id', Integer, primary_key=True)
+  Home = Column('home', Integer, ForeignKey('marathon_football_team.id'))
+  Away = Column('away', Integer, ForeignKey('marathon_football_team.id'))
+  Time = Column('time', DateTime)
+  _1 = Column('_1', Float)
+  _x = Column('_x', Float)
+  _2 = Column('_2', Float)
+  Created = Column('created', DateTime)
+
+  def __init__(self, match):
+    self.Id = match['id']
+    self.Home = Operations.GetOrCreateMarathonFootballTeam(match['home']).Id
+    self.Away = Operations.GetOrCreateMarathonFootballTeam(match['away']).Id
+    self.Time = match['time']
+    self._1 = match['_1']
+    self._x = match['_x']
+    self._2 = match['_2']
+    self.Created =  datetime.now().replace(microsecond=0)
+
+class Operations:
+
+  def QueryMarathonFootballTeam():
+    return session.query(MarathonFootballTeam).all()
+
+  def GetOrCreateMarathonFootballTeam(team_name):
+
+    if team_name in MARATHON_FOOTBALL_TEAM_CACHE:
+      return MARATHON_FOOTBALL_TEAM_CACHE[team_name]
+
+    else:
+      team = MarathonFootballTeam(team_name)
+      session.add(team)
+      session.commit()
+      MARATHON_FOOTBALL_TEAM_CACHE[team_name] = team
+
+    return team
+
+  def SaveMarathonFootball(match):
+    if session.query(MarathonFootballMatch.Id).filter_by(Id=match['id']).scalar() == None:
+      session.add(MarathonFootballMatch(match))
+      session.commit()
+
+Base.metadata.create_all(engine)
+Session = sessionmaker()
+Session.configure(bind=engine)
+session = Session()
+MARATHON_FOOTBALL_TEAM_CACHE = {team.Value: team for team in  Operations.QueryMarathonFootballTeam()}
+if __name__ == "__main__":
+  print(os.environ.get('BETTING_DATABASE'))
+
---
+++ betting/spiders/marathon.py
@@ -0,0 +1,97 @@
+import scrapy
+from scrapy import signals
+from scrapy.crawler import CrawlerProcess
+from time import time
+from pprint import pprint
+import os
+import re
+from datetime import datetime
+
+if __name__ == '__main__':
+  from ORM import Operations
+
+else:
+  from dison.spiders.ORM import Operations
+
+MONTHS = ['', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+MONTH_DICT = {value:idx for idx, value in enumerate(MONTHS)}
+
+def parse_date(date_list):
+  date = datetime.now()
+  if len(date_list) == 1:
+    hour, minute = date_list[0].split(":")
+    return datetime(date.year, date.month, date.day, int(hour), int(minute), 0)
+
+  elif len(date_list) == 3:
+    day, month, time = date_list
+    month = MONTH_DICT[month]
+    hour, minute = time.split(":")
+
+    return datetime(date.year, month, int(day), int(hour), int(minute), 0)
+
+  return date
+
+class RootSpider(scrapy.Spider):
+  name = "root"
+  football_matches = []
+
+  def start_requests(self):
+    start_urls = ['https://www.marathonbet.com/en/all-events.htm?cpcids=all']
+
+    for url in start_urls:
+      yield scrapy.Request(url=url,
+        callback=self.start_page_parser,
+        errback=self.errbacktest,
+        meta={'root': url})
+
+  def start_page_parser(self, response):
+    #football
+    football_urls = [x for x in response.xpath("//td/a[@class='category-label-link']/@href").extract()
+      if 'betting/Football/' in x]
+
+    for football_url in football_urls:
+      yield response.follow(url=football_url,
+        callback=self.football,
+        errback=self.errbacktest)
+
+  def football(self, response):
+    events = response.xpath("//div[@data-event-treeid]")
+
+    matches = []
+    for event in events:
+      try:
+        match = {}
+        match['id'] = event.xpath(".//@data-event-treeid").extract_first()
+        match['home'], match['away'] = event.xpath(".//span[@data-member-link]/text()").extract()
+
+        match['time'] = event.xpath(".//td[@class='date']/text()").extract_first().split()
+        match['time'] = parse_date(match['time'])
+
+        match['_1'], match['_x'], match['_2'] = event.xpath(
+          ".//span[@data-selection-price]/text()").extract()[0:3]
+
+      except Exception as e:
+        print(response.url, e)
+
+      self.football_matches.append(match)
+
+  def errbacktest(self, failiure):
+    pass
+
+  @classmethod
+  def from_crawler(cls, crawler, *args, **kwargs):
+    spider = super().from_crawler(crawler, *args, **kwargs)
+    crawler.signals.connect(spider.spider_closed, signal=signals.spider_closed)
+    return spider
+
+  def spider_closed(self, spider):
+    [Operations.SaveMarathonFootball(football_match) for football_match in self.football_matches]
+
+if __name__ == "__main__":
+  process = CrawlerProcess({
+      'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)',
+      'LOG_LEVEL': 'ERROR'
+  })
+
+  process.crawl(RootSpider)
+  process.start()
